51914fa3cb0e8225312238e89ce9a88a605ad108
Ohgidi <ohgidi@yahoo.com>
2022-08-07 14:51:31 -0700

    my third commit

--- _printf.c
+++ _printf.c
@@ -24,6 +24,10 @@ int _printf(const char *format, ...)
 					printed += _print_char(args);
 					i += 2;
 					break;
+				case 's':
+					printed += _print_str(args);
+					i += 2;
+					break;
 				default:
 					_putchar(format[i]);
 					_putchar(format[i + 1]);
