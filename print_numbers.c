118adbf6fed42ad50c92e7c3357d282b1af190cb
Ghumdia <bosass80@gmail.com>
2022-08-09 11:34:07 -0700

    Committed

---
+++ print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_i - prints integer
+ * @i: integer
+ *
+ * Return: number of chars printed
+ */
+
+int print_i(va_list i)
+{
+	int a[10];
+	int j, m, n, sum, count;
+
+	n = va_arg(i, int);
+	coun
+}
